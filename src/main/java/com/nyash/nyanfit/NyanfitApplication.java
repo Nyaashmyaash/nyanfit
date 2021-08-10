76a7bebdb41c58bdc21ed55d80157ecf423d570d
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-11 03:46:06 +0700

    Initial commit

---
+++ src/main/java/com/nyash/nyanfit/NyanfitApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.nyanfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyanfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyanfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/nyanfit/NyanfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.nyanfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyanfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
